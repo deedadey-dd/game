4428852f6fac74a71b7e1aa05f0d88569dc487cc
Dennis Dadey <deedadey@gmail.com>
2025-02-06 11:05:38 +0000

    20250206

--- deal_or_no_deal/users/urls.py
+++ deal_or_no_deal/users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import api_views
 
 urlpatterns = [
-    path('user/register/', api_views.UserRegistrationView.as_view(), name='api_register'),
-    path('user/login/', api_views.login_view, name='api_login'),
-    path('user/password-reset/', api_views.RequestPasswordResetView.as_view(), name='password_reset_request'),
-    path('user/password-reset-confirm/<str:uidb64>/<str:token>/', api_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('user/token/', api_views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', api_views.UserRegistrationView.as_view(), name='api_register'),
+    path('login/', api_views.login_view, name='api_login'),
+    path('password-reset/', api_views.RequestPasswordResetView.as_view(), name='password_reset_request'),
+    path('password-reset-confirm/<str:uidb64>/<str:token>/', api_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('token/', api_views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 ]
